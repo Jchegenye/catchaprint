b553941ebb06a7bc07b26c91e8111db8df78bfae
Jackson Asumu Chegenye <chegenyejackson@gmail.com>
2020-04-01 13:22:20 +0300

    wider updates

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-// server.js
+// // server.js
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
+app.use(serveStatic(__dirname + "/src/build"));
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('server started ' + port);
+
+var express = require('express');
+var history = require('connect-history-api-fallback');
+var app = express();
+
+// Middleware for serving '/dist' directory
+const staticFileMiddleware = express.static('build');
